5ddb9aa5398ae957a1a3690ad314ea833ac08b9f
benny_neiman <bn161299@gmail.com>
2026-02-24 12:47:50 +0200

    add pargrefe

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ export default function App() {
   return (
     <>
       <Header/>
-      {/* <CardContainer/>
-      <p></p>
-      <Board/>
-      <RestartButtom/> */}
+      {/* <CardContainer/> */}
+      <p className='bg-[#1a2537] text-[#7f8a9e] text-center border-t-1 border-b-1 border-[#597199] font-bold pt-4 pb-4'>Locate and neutralize all bombs before time runs out.</p>
+      {/* <Board/> */}
+      {/* <RestartButtom/> */}
     </>
   )
 }
