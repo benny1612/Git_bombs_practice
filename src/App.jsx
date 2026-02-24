a13c868d47824c1a5a79f562e49ed932431aeecb
benny_neiman <bn161299@gmail.com>
2026-02-24 16:03:06 +0200

    bord

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Board from './comps/Board'
 import { useState } from 'react'
 
 export default function App() {
-  const { timer, seconds, isActive, toggle, reset } = useTimer(180)
+  // const { timer, seconds, isActive, toggle, reset } = useTimer(180)
   return (
     <div className='background content-between flex flex-col'>
       <Header/>
-      <CardContainer timer={timer} />
+      {/* <CardContainer timer={timer} /> */}
       <p className='bg-[#1a2537] text-[#7f8a9e] text-center border-t-1 border-b-1 border-[#597199] font-bold pt-4 pb-4'>Locate and neutralize all bombs before time runs out.</p>
       <Board/>
       {/* <RestartButtom/> */}
